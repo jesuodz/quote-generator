2697d556be88b9132e51390f8303a647bb3f601d
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-08-11 22:41:29 -0400

    Delete all components

--- src/js/app.js
+++ src/js/app.js
@@ -2,58 +2,15 @@ const e = React.createElement;
 const url = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
 const urlCORS = 'https://cors-anywhere.herokuapp.com/' + url;
 
-class QuoteText extends React.Component {
-    constructor() {
-        super();
-        this.state = {text: 'Lorem'};
-    }
-    render() {
-        return e('span', {className: 'text'}, this.state.text);
-    }
-}
-
-class QuoteAuthor extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            author: this.props.children.author
-        };
-    }
-    
-    render() {
-        return e('span', {className: 'author'}, this.state.author);
-    }
-}
-
-class newQuoteBtn extends React.Component {
-    constructor() {
-        super();
-        this.state = {newQuote: 'Lorem'};
-    }
-    render() {
-        return e('button', {className: 'new-quote'}, this.state.newQuote);
-    }
-}
-
-class tweetQuoteBtn extends React.Component {
-    constructor() {
-        super();
-        this.state = {tweetQuote: 'Lorem'};
-    }
-    render() {
-        return e('a', {className: 'tweet-quote'}, this.state.tweetQuote);
-    }
-}
-
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: "please ",
-            author: "wait"
+            text: null,
+            author: null
         }
-        }
-
+    }
+    
     componentDidMount() {
         this.getData();
     }
@@ -71,10 +28,10 @@ class App extends React.Component {
 
     render() {
         return [
-            e(QuoteText, {key: "quote"} , {text: this.state.text}), 
-            e(QuoteAuthor, {key: "author"}, {author: this.state.author}),
-            e(newQuoteBtn, {key: "new-quote"}, {newQuote: 'New Quote'}),
-            e(tweetQuoteBtn, {key: "tweet"}, {tweetQuote: 'Tweet!'})
+            e('span', {key: 'quote', className: 'text'}, this.state.text),
+            e('span', {key: 'author', className: 'author'}, this.state.author),
+            e('button', {key: 'new-quote', className: 'new-quote'}, 'New quote'),
+            e('a', {key: 'tweet-quote', className: 'tweet-quote'}, this.state.tweetQuote)
         ];
     }
 }
